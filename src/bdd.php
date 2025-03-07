09c3c4b3ccfbfc0bfd9af984e7156e5392cf886d
Hugo <hugomahe53100@gmail.com>
2025-03-07 16:35:05 +0100

    try to add env var

--- src/bdd.php
+++ src/bdd.php
@@ -1,25 +1,20 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 
-use Dotenv\Dotenv;
+$config = require('/../config.php');
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-function getDb()
+function getDb($config)
 {
-    $host = $_ENV['DB_HOST'];
-    $dbname = $_ENV['DB_NAME'];
-    $user = $_ENV['DB_USER'];
-    $pass = $_ENV['DB_PASS'];
+    $host = $config['DB_HOST'];
+    $dbname = $config['DB_NAME'];
+    $user = $config['DB_USER'];
+    $pass = $config['DB_PASS'];
 
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
-    } catch(PDOException $e) {
-        echo "La connexion a échoué " . $e->getMessage();
+    } catch (PDOException $e) {
+        echo "La connexion a échoué : " . $e->getMessage();
         return null;
     }
 }
-?>
--- src/register.php
+++ src/register.php
@@ -2,14 +2,16 @@
 session_start();
 require_once('bdd.php');
 
-if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password_confirm'])) {
+$config = require(__DIR__ . '/../config.php');
+
+if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password_confirm'])) {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $password_confirm = $_POST['password_confirm'];
 
-    if($password === $password_confirm) {
-        $connexion = getDb();
+    if ($password === $password_confirm) {
+        $connexion = getDb($config);
         $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
         $connexion->exec($sql);
         header('Location: index.php');
@@ -20,7 +22,9 @@ if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password
 ?>
 
 <html>
+
 <head>
+
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -30,36 +34,38 @@ if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     </head>
 </head>
+
 <body>
-<div class="container">
-    <h1>Inscription :</h1>
-    <?php if(!empty($error)){ ?>
-        <?php foreach($error as $e){ ?>
-            <div class="alert alert-danger">
-                <?= $e ?>
-            </div>
+    <div class="container">
+        <h1>Inscription :</h1>
+        <?php if (!empty($error)) { ?>
+            <?php foreach ($error as $e) { ?>
+                <div class="alert alert-danger">
+                    <?= $e ?>
+                </div>
+            <?php } ?>
         <?php } ?>
-    <?php } ?>
-    <form action="register.php" method="POST">
-        <div class="form-group">
-            <label for="username">Nom d'utilisateur :</label>
-            <input type="text" class="form-control" name="username" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Email :</label>
-            <input type="email" class="form-control" name="email" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Mot de passe :</label>
-            <input type="password" class="form-control" name="password" required>
-        </div>
-        <div class="form-group">
-            <label for="password_confirm">Confirmer le mot de passe :</label>
-            <input type="password" class="form-control" name="password_confirm" required>
-        </div>
-        <button type="submit" class="btn btn-primary">S'inscrire</button>
-        <a href="index.php">Se connecter</a>
-    </form>
-</div>
+        <form action="register.php" method="POST">
+            <div class="form-group">
+                <label for="username">Nom d'utilisateur :</label>
+                <input type="text" class="form-control" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email :</label>
+                <input type="email" class="form-control" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe :</label>
+                <input type="password" class="form-control" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="password_confirm">Confirmer le mot de passe :</label>
+                <input type="password" class="form-control" name="password_confirm" required>
+            </div>
+            <button type="submit" class="btn btn-primary">S'inscrire</button>
+            <a href="index.php">Se connecter</a>
+        </form>
+    </div>
 </body>
+
 </html>
