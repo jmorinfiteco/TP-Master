0c82529048b7b1cd2b7195a67ea359a3d69eb312
Hugo <hugomahe53100@gmail.com>
2025-03-07 14:52:41 +0100

    fix injection sql

--- src/deleteAccount.php
+++ src/deleteAccount.php
@@ -1,23 +1,36 @@
 <?php
 session_start();
 require_once('bdd.php');
-if(isset($_GET['id'])) {
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+
+if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
     $connexion = getDb();
-    $sql = "SELECT * FROM users WHERE id = '$id'";
+
+    $sql = "SELECT * FROM users WHERE id = :id";
     $req = $connexion->prepare($sql);
+    $req->bindParam(':id', $id, PDO::PARAM_INT);
     $req->execute();
     $user = $req->fetch();
-    if(empty($user)) {
-        echo "Il n'y a pas d'utilisateur avec cet id";
+
+    if (empty($user)) {
+        header('Location: error.php?code=no_user');
+        exit();
     } else {
-        $sql = "DELETE FROM users WHERE id = '$id'";
+        $sql = "DELETE FROM users WHERE id = :id";
         $req = $connexion->prepare($sql);
+        $req->bindParam(':id', $id, PDO::PARAM_INT);
         $req->execute();
+
         session_destroy();
         header('Location: index.php');
+        exit();
     }
 } else {
     header('Location: index.php');
+    exit();
 }
-
--- src/index.php
+++ src/index.php
@@ -7,43 +7,48 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $password = $_POST['password'];
 
     $connexion = getDb();
-    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+
+    $sql = "SELECT * FROM users WHERE email = :email AND password = :password";
     $req = $connexion->prepare($sql);
+    $req->bindParam(':email', $email, PDO::PARAM_STR);
+    $req->bindParam(':password', $password, PDO::PARAM_STR);
     $req->execute();
     $user = $req->fetch();
 
-    if(!empty($user)){
+    if (!empty($user)) {
         $_SESSION['user'] = $user;
-        header('Location: profil.php?id='.$user['id']);
+        header('Location: profil.php?id=' . $user['id']);
+        exit();
     }
 }
 ?>
 <html>
+
 <head>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Megacasting - Connexion</title>
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    </head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Megacasting - Connexion</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
+
 <body>
-<div class="container">
-    <h1>Connexion :</h1>
-    <form action="/" method="POST">
-        <div class="form-group">
-            <label for="email">Email :</label>
-            <input type="email" class="form-control" name="email" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Mot de passe :</label>
-            <input type="password" class="form-control" name="password" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Connexion</button>
-        <a href="register.php">S'inscrire</a>
-    </form>
-</div>
+    <div class="container">
+        <h1>Connexion :</h1>
+        <form action="/" method="POST">
+            <div class="form-group">
+                <label for="email">Email :</label>
+                <input type="email" class="form-control" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe :</label>
+                <input type="password" class="form-control" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Connexion</button>
+            <a href="register.php">S'inscrire</a>
+        </form>
+    </div>
 </body>
+
 </html>
