23ee0940acfe2934b53da92eb901dde63bc16196
Joachim Guitton <joachim.guitton@gmail.com>
2025-03-07 16:12:48 +0100

    fix : mise en place de la lecture des identifiants et des mots de passe dans le fichier bdd.php.

--- src/bdd.php
+++ src/bdd.php
@@ -1,8 +1,20 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 function getDb()
 {
+    $host = $_ENV['DB_HOST'];
+    $dbname = $_ENV['DB_NAME'];
+    $user = $_ENV['DB_USER'];
+    $pass = $_ENV['DB_PASS'];
+
     try {
-        $conn = new PDO("mysql:host=db;dbname=app_db", 'app_user', 'Not24get');
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch(PDOException $e) {
